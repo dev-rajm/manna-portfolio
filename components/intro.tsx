18e9545a8920ad9402091d0be5a369ae936e485b
rajmanna <108714991+dev-rajm@users.noreply.github.com>
2025-05-13 19:08:25 +0530

    update intro cards

--- components/intro.tsx
+++ components/intro.tsx
@@ -17,20 +17,20 @@ const IntroComponent = () => {
           <CountComponent count={20} label="Worldwide clients" />
         </div>
       </div>
-      <div className="flex text-3xl font-medium uppercase flex-col lg:flex-row gap-5 mt-20">
-        <div className="flex flex-col text-black px-5 py-5 rounded-xl bg-[url(/bg1.svg)] bg-cover bg-center">
+      <div className="flex text-2xl font-medium uppercase flex-col lg:flex-row gap-5 mt-20">
+        <div className="flex flex-2/3 flex-col px-5 pb-5 pt-10 rounded-xl bg-[url(/b1.svg)] bg-cover">
           <Layers size={36} />
-          <p className="mt-6 mb-3">Dynamic & Responsive Design</p>
-          <div className="self-end border border-black hover:bg-black hover:text-white w-fit rounded-sm p-1">
+          <p className="my-5">Dynamic Functionality, Responsive Design</p>
+          <div className="self-end border hover:bg-white hover:text-accent-foreground w-fit rounded-sm p-1">
             <ArrowUpRight />
           </div>
         </div>
-        <div className="flex flex-col px-5 py-5 rounded-xl bg-[url(/bg2.svg)] bg-cover bg-center">
+        <div className="flex flex-col px-5 pb-5 pt-10 rounded-xl bg-[url(/b2.svg)] text-black bg-cover">
           <PanelsTopLeft size={36} />
-          <p className="mt-6 mb-3">
-            Typescript, NextJS, ReactJS, Prisma, Docker
+          <p className="my-5">
+            Typescript, NextJS, ReactJS, Prisma ORM, Docker, TurboRepo
           </p>
-          <div className="self-end border hover:bg-white hover:text-black w-fit rounded-sm p-1">
+          <div className="self-end border border-black hover:bg-black hover:text-white w-fit rounded-sm p-1">
             <ArrowUpRight />
           </div>
         </div>
